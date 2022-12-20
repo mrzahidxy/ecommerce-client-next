de7d9fdf68c95a9056910455c3950100261431a9
Zahid <mrzahidxy@gmail.com>
2022-12-20 17:54:50 +0600

    fix: dynamic home page data

--- pages/comps/categories/Categories.jsx
+++ pages/comps/categories/Categories.jsx
@@ -1,66 +1,32 @@
 import React from "react";
 
 const Categories = () => {
+  const items = [
+    { id: 1, name: "Chair", quantity: 10 },
+    { id: 1, name: "Sofa", quantity: 12 },
+    { id: 1, name: "Bedroom", quantity: 23 },
+    { id: 1, name: "Table", quantity: 15 },
+    { id: 1, name: "Chair", quantity: 10 },
+    { id: 1, name: "Sofa", quantity: 12 },
+  ];
+
   return (
     <div className="container py-8">
       <h1 className="text-2xl text-gray-800 font-medium mb-5 uppercase">
         Shop By Category
       </h1>
       <div className="grid grid-cols-2 md:grid-cols-3 gap-10">
-        <div className="relative rounded-smoverflow-hidden group">
-          <img
-            className="w-full"
-            src="https://media.istockphoto.com/id/1293762741/photo/modern-living-room-interior-3d-render.jpg?b=1&s=170667a&w=0&k=20&c=Ko289acalSrEKwYI5sb20u2WQa7KS9L-_0Ug4u2iEmc="
-          />
-          <span className="absolute inset-0 bg-black bg-opacity-30 font-medium text-white flex justify-center items-center group-hover:bg-opacity-50 transition">
-            Bedroom
-          </span>
-        </div>
-        <div className="relative rounded-smoverflow-hidden group">
-          <img
-            className="w-full"
-            src="https://media.istockphoto.com/id/1293762741/photo/modern-living-room-interior-3d-render.jpg?b=1&s=170667a&w=0&k=20&c=Ko289acalSrEKwYI5sb20u2WQa7KS9L-_0Ug4u2iEmc="
-          />
-          <span className="absolute inset-0 bg-black bg-opacity-30 font-medium text-white flex justify-center items-center group-hover:bg-opacity-50 transition">
-            Bedroom
-          </span>
-        </div>
-        <div className="relative rounded-smoverflow-hidden group">
-          <img
-            className="w-full"
-            src="https://media.istockphoto.com/id/1293762741/photo/modern-living-room-interior-3d-render.jpg?b=1&s=170667a&w=0&k=20&c=Ko289acalSrEKwYI5sb20u2WQa7KS9L-_0Ug4u2iEmc="
-          />
-          <span className="absolute inset-0 bg-black bg-opacity-30 font-medium text-white flex justify-center items-center group-hover:bg-opacity-50 transition">
-            Bedroom
-          </span>
-        </div>
-        <div className="relative rounded-smoverflow-hidden group">
-          <img
-            className="w-full"
-            src="https://media.istockphoto.com/id/1293762741/photo/modern-living-room-interior-3d-render.jpg?b=1&s=170667a&w=0&k=20&c=Ko289acalSrEKwYI5sb20u2WQa7KS9L-_0Ug4u2iEmc="
-          />
-          <span className="absolute inset-0 bg-black bg-opacity-30 font-medium text-white flex justify-center items-center group-hover:bg-opacity-50 transition">
-            Bedroom
-          </span>
-        </div>
-        <div className="relative rounded-smoverflow-hidden group">
-          <img
-            className="w-full"
-            src="https://media.istockphoto.com/id/1293762741/photo/modern-living-room-interior-3d-render.jpg?b=1&s=170667a&w=0&k=20&c=Ko289acalSrEKwYI5sb20u2WQa7KS9L-_0Ug4u2iEmc="
-          />
-          <span className="absolute inset-0 bg-black bg-opacity-30 font-medium text-white flex justify-center items-center group-hover:bg-opacity-50 transition">
-            Bedroom
-          </span>
-        </div>
-        <div className="relative rounded-smoverflow-hidden group">
-          <img
-            className="w-full"
-            src="https://media.istockphoto.com/id/1293762741/photo/modern-living-room-interior-3d-render.jpg?b=1&s=170667a&w=0&k=20&c=Ko289acalSrEKwYI5sb20u2WQa7KS9L-_0Ug4u2iEmc="
-          />
-          <span className="absolute inset-0 bg-black bg-opacity-30 font-medium text-white flex justify-center items-center group-hover:bg-opacity-50 transition">
-            Bedroom
-          </span>
-        </div>
+        {items.map((item) => (
+          <div className="relative rounded-smoverflow-hidden group">
+            <img
+              className="w-full"
+              src="https://media.istockphoto.com/id/1293762741/photo/modern-living-room-interior-3d-render.jpg?b=1&s=170667a&w=0&k=20&c=Ko289acalSrEKwYI5sb20u2WQa7KS9L-_0Ug4u2iEmc="
+            />
+            <span className="absolute inset-0 bg-black bg-opacity-30 font-medium text-white flex justify-center items-center group-hover:bg-opacity-50 transition">
+              {item.name}
+            </span>
+          </div>
+        ))}
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,61 @@ import Navbar from "./comps/navbar/Navbar";
 import Product from "./comps/products/Product";
 
 export default function Home() {
+  const products = [
+    {
+      id: 1,
+      name: "Wooden Chair",
+      price: 2000,
+      img: "https://target.scene7.com/is/image/Target/Chairs_QUIVER-210420-1618938782938",
+    },
+    {
+      id: 2,
+      name: "Steel Chair",
+      price: 2000,
+      img: "https://img.zcdn.com.au/lf/50/hash/38454/19055758/4/Etienne+Velvet+Dining+Chairs.jpg",
+    },
+    {
+      id: 3,
+      name: "Wooden Bed",
+      price: 2000,
+      img: "https://d3home.com/wp-content/uploads/2019/04/b3e3568a-d0b5-44b0-922d-0d18e7b0c67e.jpg",
+    },
+    {
+      id: 4,
+      name: "Wooden Chair",
+      price: 2000,
+      img: "https://target.scene7.com/is/image/Target/Chairs_QUIVER-210420-1618938782938",
+    },
+  ];
+
+  const randomProducts = [
+    
+    {
+      id: 5,
+      name: "Wooden Chair",
+      price: 2000,
+      img: "https://target.scene7.com/is/image/Target/Chairs_QUIVER-210420-1618938782938",
+    },
+    {
+      id: 6,
+      name: "Steel Chair",
+      price: 2000,
+      img: "https://img.zcdn.com.au/lf/50/hash/38454/19055758/4/Etienne+Velvet+Dining+Chairs.jpg",
+    },
+    {
+      id: 7,
+      name: "Wooden Bed",
+      price: 2000,
+      img: "https://d3home.com/wp-content/uploads/2019/04/b3e3568a-d0b5-44b0-922d-0d18e7b0c67e.jpg",
+    },
+    {
+      id: 8,
+      name: "Wooden Bed",
+      price: 2000,
+      img: "https://d3home.com/wp-content/uploads/2019/04/b3e3568a-d0b5-44b0-922d-0d18e7b0c67e.jpg",
+    }
+
+  ];
   return (
     <div>
       <Head>
@@ -22,7 +77,8 @@ export default function Home() {
         <Banner />
         <Feature />
         <Categories />
-        <Product />
+        <Product products={products} title="New Arrivals" />
+        <Product products={randomProducts} title="Random" />
       </main>
     </div>
   );
