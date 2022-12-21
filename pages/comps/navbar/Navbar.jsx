5a9bec96b2681c077c574ecb9429ac65349323ce
Zahid <mrzahidxy@gmail.com>
2022-12-21 17:53:20 +0600

    fix: nav responsive

--- pages/comps/navbar/Navbar.jsx
+++ pages/comps/navbar/Navbar.jsx
@@ -6,41 +6,45 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="bg-gray-800">
-      <div className="container text-white p-3 md:hidden">
+      {/* <div className="container text-white p-3 md:hidden">
         <FontAwesomeIcon className="4xl" icon={faBars} />
-      </div>
-      <div className="hidden container md:flex md:flex-row ">
+      </div> */}
+      <div className="container flex  ">
         <div className="bg-primary text-white py-4 px-3 flex gap-2 items-center cursor-pointer transition">
           <FontAwesomeIcon icon={faBars} />
-          All Categories
+
+          <span className="tetx-sm md:text-base">All Categories</span>
         </div>
         <div className="flex items-center justify-between flex-grow pl-10 ">
           <div className="flex items-center space-x-6 capitalize ">
-            <Link href="/" className="text-white hover:text-primary transition">
+            <Link
+              href="/"
+              className="text-white text-sm md:text-base hover:text-primary transition"
+            >
               Home
             </Link>
             <Link
               href="/shop"
-              className="text-white hover:text-primary transition"
+              className="text-white text-sm md:text-base  hover:text-primary transition"
             >
               Shop
             </Link>
             <Link
               href="/about"
-              className="text-white hover:text-primary transition"
+              className="text-white text-sm md:text-base hover:text-primary transition"
             >
-              About Us
+              About
             </Link>
             <Link
               href="/contact"
-              className="text-white hover:text-primary transition"
+              className="text-white text-sm md:text-base hover:text-primary transition"
             >
-              Contact Us
+              Contact
             </Link>
           </div>
           <Link
             href="login"
-            className="text-white hover:text-primary transition"
+            className="hidden text-white text-sm md:text-base hover:text-primary transition"
           >
             Login/Register
           </Link>
