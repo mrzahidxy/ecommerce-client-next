4d19d38cc0c579a67eb91f47a9c2e8bdaee7d5c6
Zahid <mrzahidxy@gmail.com>
2023-01-04 12:06:09 +0600

    fix: custom fetch function and protect admin route

--- pages/admin/user/index.jsx
+++ pages/admin/user/index.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AdminLayout from "../../../comps/layout/AdminLAyout";
-import useSWR from "swr";
-import axios from "axios";
 import { Table } from "antd";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useFetcher } from "../../../customhooks/useFetcher";
 
 const columns = [
   {
@@ -19,28 +20,33 @@ const columns = [
     title: "Role",
     dataIndex: "isAdmin",
     key: "isAdmin",
+    render: (isAdmin) => <>{isAdmin ? "Admin" : "User"}</>,
   },
 ];
 
-const fetcher = (url) =>
-  axios
-    .get(url, {
-      headers: {
-        token:
-          "Bearer " +
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMDQyNmNmMzIzZmQxNTc0NzM5OGY1NCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3MjU3MTIzNiwiZXhwIjoxNjcyODMwNDM2fQ.l21nvnrntq6c8IaqRX5JhjsBLV_bseU0ZoAWAY8-CBI",
-      },
-    })
-    .then((res) => res.data);
-
 const User = () => {
-  const { data, error } = useSWR(
-    ["https://ecommerce-mern-api.vercel.app/api/users/"],
-    fetcher
-  );
-  const users = data;
+  const session = useSession();
+  const router = useRouter();
 
-  console.log(data);
+  useEffect(() => {
+    if (session?.data?.isAdmin !== true) {
+      router.push("/admin/login");
+    }
+  }, []);
+
+  const { data, IsError, isLoading } = useFetcher([
+    "users",
+    session?.data?.accessToken,
+  ]);
+  const users = data;
+  users?.forEach(function (element) {
+    element.key = Math.random();
+  });
+
+  // console.log(data);
+
+  if (IsError) return <div>failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
 
   return <Table columns={columns} dataSource={users} />;
 };
